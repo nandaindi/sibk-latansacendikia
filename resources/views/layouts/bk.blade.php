add7a1eae3003d8e62f878e010b2f26b225354fd
kdpras00 <kdpras00@gmail.com>
2026-07-08 21:27:09 +0700

    Add missing csrf token meta tag

--- resources/views/layouts/bk.blade.php
+++ resources/views/layouts/bk.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'BK – Bimbingan Konseling')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="shortcut icon" href="{{ asset('img/latansaico.png') }}" type="image/x-icon">
