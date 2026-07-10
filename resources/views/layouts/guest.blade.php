e1356179e937e3dcdc57a50bff1c709fb92ecece
kdpras00 <kdpras00@gmail.com>
2026-07-11 00:51:15 +0700

    feat: add csrf-token meta tag to guest layout header

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Bimbingan Konseling')</title>
     <link rel="shortcut icon" href="{{ asset('img/latansaico.png') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
